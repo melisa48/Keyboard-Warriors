85118ca8a7342cb0dfc6187f1b260ff90e98db96
Jay Gupta <jaygupt1973@gmail.com>
2023-03-16 20:18:07 -0700

    add HTTP errors

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const createError = require("http-errors");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const rootRoutes = require("./routes/root.js");
 
 app.use("/", rootRoutes);
+
+app.use((request, response, next) => {
+  next(createError(404));
+});
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
