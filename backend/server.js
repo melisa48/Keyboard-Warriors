78c44f380632bae9843e70c91a404ab0ab02b7cc
Jay Gupta <jaygupt1973@gmail.com>
2023-03-17 00:12:11 -0700

    serve static files; add favicon

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const createError = require("http-errors");
 const rootRoutes = require("./routes/root");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use(express.static(path.join(__dirname, "static")));
 app.use("/", rootRoutes);
 app.use((request, response, next) => {
   next(createError(404));
