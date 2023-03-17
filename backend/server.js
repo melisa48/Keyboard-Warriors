fd87297cd7b9cba825e84d433fed97c1bf27e468
Jay Gupta <jaygupt1973@gmail.com>
2023-03-17 12:56:42 -0700

    add livereload

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,20 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+if (process.env.NODE_ENV === "development") {
+  const liveReload = require("livereload");
+  const connectLiveReload = require("connect-livereload");
+
+  const liveReloadServer = liveReload.createServer();
+  liveReloadServer.watch(path.join(__dirname, "static"));
+  liveReloadServer.server.once("connection", () => {
+    setTimeout(() => {
+      liveReloadServer.refresh("/");
+    }, 100);
+  });
+
+  app.use(connectLiveReload());
+}
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
