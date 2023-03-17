b975c16081eaa4c5369b937431c66c1f415171f0
Jay Gupta <jaygupt1973@gmail.com>
2023-03-16 18:55:06 -0700

    add routes

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (request, response) => {
-  response.send("Hello World!");
-});
+const rootRoutes = require("./routes/root.js");
+
+app.use("/", rootRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
