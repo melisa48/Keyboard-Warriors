3d11531d4acbd043f8848b76dab4ebb7929fe5fb
Jay Gupta <jaygupt1973@gmail.com>
2023-04-12 13:39:52 -0700

    create game_users migration

---
+++ migrations/1681330930438_game-users-migration.js
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+
+/**
+ *
+ * @param {import("node-pg-migrate/dist/types").MigrationBuilder} pgm
+ */
+exports.up = (pgm) => {
+  pgm.createTable("game_users", {
+    game_id: {
+      type: "integer",
+      references: "games(id)",
+    },
+    user_id: {
+      type: "integer",
+      references: "users(id)",
+    },
+    score: {
+      type: "integer",
+      default: 0,
+    },
+    current: {
+      type: "boolean",
+    },
+    play_order: {
+      type: "integer",
+    },
+  });
+};
+
+/**
+ * @param {import("node-pg-migrate/dist/types").MigrationBuilder} pgm
+ */
+exports.down = (pgm) => {
+  pgm.dropTable("game_users");
+};
