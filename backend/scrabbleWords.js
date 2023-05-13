882f48013d13891e55cdcd68f97ebe74cd5eaec9
Jay Gupta <jaygupt1973@gmail.com>
2023-05-13 03:41:55 -0700

    remove load Scrabble words message

--- backend/scrabbleWords.js
+++ backend/scrabbleWords.js
@@ -5,7 +5,6 @@ let scrabbleWords;
 
 function loadScrabbleWords() {
   if (!scrabbleWords) {
-    console.log("LOADING THE SCRABBLE WORDS");
     const data = fs.readFileSync(path.join(__dirname + "/words.txt"), "utf-8");
     scrabbleWords = new Set(data.trim().split("\n"));
   }
